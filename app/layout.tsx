204b3dfabb68cb01a1e882e1016cba7ebe1c9839
OndrejHj04 <ondra.hajku@seznam.cz>
2023-07-09 12:34:51 +0200

    ui breakpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,55 +1,8 @@
-"use client";
-import React, { use } from "react";
 import "./globals.css";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { getUsers } from "../services/users";
-import { UserDB } from "../types/types";
-
-const navigation = [
-  { name: "Create user", slug: "/create-user" },
-  { name: "Edit user", slug: "/edit-user" },
-  { name: "History", slug: "/history" },
-];
-
 const RootLayout = ({ children }) => {
-  const url = usePathname();
-  const users: UserDB[] = use(getUsers());
-
   return (
     <html>
-      <body>
-        <h1 className="text-3xl font-semibold mt-16 ml-8 mb-8">
-          A home for all of our {users.length} clients
-        </h1>
-        <nav className="shadow-xl">
-          <div className="flex gap-5 ml-8">
-            <Link
-              href={"/"}
-              className={`${
-                url === "/" && "border-b-4 border-orange-400"
-              } text-xl`}
-            >
-              Dashboard
-            </Link>
-            {navigation.map(({ name, slug }) => (
-              <Link
-                key={name}
-                href={slug}
-                className={`${
-                  url.includes(slug) && "border-b-4 border-orange-400"
-                } text-xl`}
-              >
-                {name}
-              </Link>
-            ))}
-          </div>
-
-          <div className="bg-gray-200 h-0.5 mt-5 " />
-        </nav>
-
-        <div className="mx-8 mt-8">{children}</div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
-"use client";
-
-import Link from "next/link";
-import { use, useEffect, useState } from "react";
-import DeletePopup from "../components/Delete";
-import { UserDB } from "../types/types";
-import { getUsers } from "../services/users";
-import UserCard from "../components/UserCard";
-
 const Home = () => {
-  const users: UserDB[] = use(getUsers());
-
   return (
     <>
-      <div className="grid-cols-3 grid justify-items-center max-w-5xl">
-        {users.map((user) => (
-          <UserCard user={user}/>
-        ))}
-      </div>
+      <div>Home</div>
     </>
   );
 };

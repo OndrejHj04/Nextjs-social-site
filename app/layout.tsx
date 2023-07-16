c55d224e30e194f617e2457b503507fdbddd75b6
OndrejHj04 <ondra.hajku@seznam.cz>
2023-07-16 18:59:38 +0200

    register form

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,16 @@ import { Providers } from "../providers/provider";
 import AuthProvider from "../providers/auth-provider";
 import TopBar from "../components/TopBar";
 import ComponentsWrapper from "../providers/ComponentsWrapper";
+import { StyledEngineProvider } from "@mui/material";
 const RootLayout = ({ children }: { children: any }) => {
   return (
     <html>
-      <body>
-        <AuthProvider>
-          <Providers>{children}</Providers>
-        </AuthProvider>
+      <body id="root">
+        <StyledEngineProvider injectFirst>
+          <AuthProvider>
+            <Providers>{children}</Providers>
+          </AuthProvider>
+        </StyledEngineProvider>
       </body>
     </html>
   );
--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,26 +1,137 @@
 "use client";
+import { Button, TextField, Typography } from "@mui/material";
 import React from "react";
-import { useForm, FormProvider } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider, csCZ } from "@mui/x-date-pickers";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+import moment from "moment";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required("Je potřeba vyplnit uživatelské jméno")
+    .min(5, "Jméno musí mít nejméně 5 znaků"),
+  email: yup
+    .string()
+    .email("Je potřeba zadat platný email")
+    .required("Je potřeba zadat platný email"),
+  date: yup.string().required("Je potřeba zadat platné datum"),
+  password: yup
+    .string()
+    .required("Je potřeba zadat heslo")
+    .min(8, "Heslo musí mít nejméně 8 znaků"),
+});
 
 const Page = () => {
-  const methods = useForm();
-  
-  const onSubmit = (data) => {
-    console.log(data, "odesláno");
-  };
+  const {
+    register,
+    setValue,
+    handleSubmit,
 
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  moment.updateLocale("en", {
+    week: {
+      dow: 1,
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="max-w-3xl mx-auto">
-      <h3 className="font-semibold text-3xl">
-        Vytvořte si profil a přidejte si svůj lol účet!
-      </h3>
-      <FormProvider {...methods}>
-        <form
-          onSubmit={methods.handleSubmit(onSubmit)}
-          className="flex flex-col"
-        ></form>
-      </FormProvider>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full shadow-2xl border p-4 rounded-2xl"
+      >
+        <Typography className="text-3xl font-medium text-center">
+          Zaregistrujte se!
+        </Typography>
+
+        <div className="w-1/5 mx-auto h-0.5 bg-gray-300 my-3" />
+        <div className="grid grid-cols-2 gap-x-2">
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="Uživatelské jméno"
+              variant="outlined"
+              className="w-full"
+              {...register("username")}
+            />
+
+            {errors?.username?.message && (
+              <Typography className="text-red-500 font-semibold">
+                {errors.username.message}
+              </Typography>
+            )}
+          </div>
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              className="w-full"
+              {...register("email")}
+            />
+            {errors?.email?.message && (
+              <Typography className="text-red-500 font-semibold">
+                {errors.email.message}
+              </Typography>
+            )}
+          </div>
+          <div>
+            <LocalizationProvider
+              dateAdapter={AdapterMoment}
+              localeText={
+                csCZ.components.MuiLocalizationProvider.defaultProps.localeText
+              }
+            >
+              <DemoContainer components={["DatePicker"]}>
+                <DatePicker
+                  label="Datum narození"
+                  format="DD. MM. YYYY"
+                  {...register("date")}
+                  onChange={(date, valid) => {
+                    valid.validationError
+                      ? setValue("date", "")
+                      : setValue("date", moment(date).format("DD. MM. YYYY"));
+                  }}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+            {errors?.date?.message && (
+              <Typography className="text-red-500 font-semibold">
+                {errors.date.message}
+              </Typography>
+            )}
+          </div>
+          <div className="pt-2">
+            <TextField
+              id="outlined-basic"
+              label="Heslo"
+              variant="outlined"
+              type="password"
+              className="w-full"
+              {...register("password")}
+            />
+            {errors?.password?.message && (
+              <Typography className="text-red-500 font-semibold">
+                {errors.password.message}
+              </Typography>
+            )}
+          </div>
+        </div>
+        <Button variant="contained" className="mt-2" type="submit">
+          Registrovat
+        </Button>
+      </form>
     </div>
   );
 };
